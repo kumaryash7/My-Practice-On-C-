cbb3a1bae7cf01ef5187b7132d2dd5293b3f9498
Yash Kumar <kumaryash7111@gmail.com>
2021-12-07 15:20:39 +0530

    New Practice(College)

---
+++ b106.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std; 
+
+int main(){
+    signed int a = 3;
+
+    unsigned int b =8;
+    cout<<a<<b;
+
+
+    return 0;
+}
---
+++ b107.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std; 
+
+int main(){
+    int a{3};
+    auto b = 'd';   //TYPE REFERENCING (AUTOMATICALLY TAKES VALUE OF ANY DATA TYPE)
+
+  cout<<a<<endl<<b;
+
+    return 0;
+}
